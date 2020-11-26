d9747bf7157d744b7b7703d5f59b8ac1c9d5380e
gapiyka <50524296+gapiyka@users.noreply.github.com>
2020-11-26 17:47:20 +0200

    Update Notes.cpp

--- Notes.cpp
+++ Notes.cpp
@@ -302,3 +302,66 @@
 //	getchar();
 //	return 0;
 //}
+
+
+// BI-Search ALOGORYTHM (2-1) for finding first number;
+//#include <stdio.h>
+//#include <iostream>
+//
+//const int n = 10;
+//int A[n][n] = { { 6, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//				{ 0, 5, 0, 0, 0, 0, 0, 0, 0, 0 },
+//				{ 0, 0, 4, 0, 0, 0, 0, 0, 0, 0 },
+//				{ 0, 0, 0, 3, 0, 0, 0, 0, 0, 0 },
+//				{ 0, 0, 0, 0, 2, 0, 0, 0, 0, 0 },
+//				{ 0, 0, 0, 0, 0, 2, 0, 0, 0, 0 },
+//				{ 0, 0, 0, 0, 0, 0, 2, 0, 0, 0 },
+//				{ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+//				{ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+//				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 } };
+//
+//void BiSearch(int answer)
+//{
+//	int low = 0;
+//	int high = n - 1;
+//	while (low < high)
+//	{
+//		int mid = (high + low) / 2;
+//		if (answer == A[mid][mid])
+//		{
+//			printf_s("This number located at index:( %d , %d )\n", mid, mid);
+//			high = mid;
+//		}
+//		else if (answer < A[mid][mid])
+//			low = mid + 1;
+//		else if (answer > A[mid][mid])
+//			high = mid - 1;
+//		}
+//}
+//
+//void print(int A[][10], int N, int M)
+//{
+//	for (int R = 0; R < N; R++) {
+//		for (int C = 0; C < M; C++)
+//			std::cout << A[R][C];
+//		std::cout << std::endl;
+//	}
+//}
+//
+//int main()
+//{
+//	print(A, n, n);
+//	int answer;
+//	printf("Please input number\n");
+//	scanf_s("%d", &answer);
+//	if (answer < 1 || answer > 6)
+//	{
+//		printf("Sorry but your number doesnt exist in array\n");
+//		return 0;
+//	}
+//	else
+//	{
+//		BiSearch(answer);
+//		return 1;
+//	}
+//}
